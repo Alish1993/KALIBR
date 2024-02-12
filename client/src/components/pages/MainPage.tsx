eb71b2e218694074f312c5aa73ea9245602f8d61
ali <Alish1993>
2024-02-12 09:19:43 +0300

    mainday11

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -1,9 +1,32 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Box, Grid, Typography, Button, Tabs, Tab } from '@mui/material';
-import fon from '../../../public/photo_5341680620679583453_y.jpg';
+import { Tabs, Tab, Typography, Slide, Box, Grid, Button } from '@mui/material';
+import PhonelinkRingTwoToneIcon from '@mui/icons-material/PhonelinkRingTwoTone';
+import RequestQuoteTwoToneIcon from '@mui/icons-material/RequestQuoteTwoTone';
+import LocalShippingTwoToneIcon from '@mui/icons-material/LocalShippingTwoTone';
+import RedeemTwoToneIcon from '@mui/icons-material/RedeemTwoTone';
 
 const theme = {}; // Вам нужно определить тему или использовать импортированную тему
+const tabContent = [
+  {
+    label: 'Tab 1',
+    text: 'Мы готовы перевезти Ваш офис в максимально сжатые сроки.',
+    img: 'https://www.pereezd-ideal.ru/pictures/ofis/service.jpg',
+    icon: <PhonelinkRingTwoToneIcon />,
+  },
+  {
+    label: 'Tab 2',
+    text: 'Окажем профессиональную помощь в квартирном переезде – Ваше участие будет минимальным.',
+    img: 'https://www.pereezd-ideal.ru/pictures/kvart/service.jpg',
+    icon: <RequestQuoteTwoToneIcon />,
+  },
+  {
+    label: 'Tab 3',
+    text: 'Предлагаем услуги квалифицированных грузчиков для решения различных погрузо-разгрузочных задач.',
+    img: 'https://www.pereezd-ideal.ru/pictures/gruzchiki/service.jpg',
+    icon: <LocalShippingTwoToneIcon />,
+  },
+];
 
 export default function MainPage(): JSX.Element {
   const [value, setValue] = useState(0);
@@ -17,15 +40,14 @@ export default function MainPage(): JSX.Element {
       <Box
         sx={{
           p: 8,
-          backgroundImage:
-            'https://lirp.cdn-website.com/c021da43/dms3rep/multi/opt/shutterstock_1177270189-1920w.jpg',
+          backgroundImage: '', // для фото
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           height: '1000px',
         }}
       >
         <Grid container spacing={1}>
-          <Grid item xs={25} sm={25} sx={{ position: 'relative' }}>
+          <Grid item xs={12} sm={12} sx={{ position: 'relative' }}>
             <div>
               <img
                 src="https://avatars.mds.yandex.net/get-ydo/2426891/2a000001737cbadc1566f63fef5011c47f84/diploma"
@@ -39,19 +61,19 @@ export default function MainPage(): JSX.Element {
                 </Typography>
                 <ul className="text-lg mb-8">
                   <li className="flex items-center mb-4">
-                    <i className="fas fa-check text-green-500 mr-2" />
+                    <span className="text-green-500 mr-2">&#10003;</span>
                     Работаем на рынке мувинга с 2000 года
                   </li>
                   <li className="flex items-center mb-4">
-                    <i className="fas fa-check text-green-500 mr-2" />
+                    <span className="text-green-500 mr-2">&#10003;</span>
                     Современный парк грузовых машин
                   </li>
                   <li className="flex items-center mb-4">
-                    <i className="fas fa-check text-green-500 mr-2" />
+                    <span className="text-green-500 mr-2">&#10003;</span>
                     Только профессиональные грузчики
                   </li>
                   <li className="flex items-center mb-4">
-                    <i className="fas fa-check text-green-500 mr-2" />
+                    <span className="text-green-500 mr-2">&#10003;</span>
                     Прозрачные и доступные цены
                   </li>
                 </ul>
@@ -67,14 +89,6 @@ export default function MainPage(): JSX.Element {
             </div>
           </Grid>
         </Grid>
-        <div className="text-lg mt-8">
-          <p>
-            Предоставляем услуги переезда в Москве и области более 15 лет. Если Вам нужна
-            профессиональная помощь, обращайтесь! Быстро проведем предварительную оценку, подготовим
-            необходимую упаковку, выполним все подготовительные работы, погрузим имущество в
-            автотранспорт и доставим на новое место.
-          </p>
-        </div>
 
         <Box
           sx={{
@@ -86,22 +100,40 @@ export default function MainPage(): JSX.Element {
             justifyContent: 'center',
           }}
         >
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            variant="scrollable"
-            scrollButtons={false}
-            aria-label="scrollable prevent tabs example"
-          >
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
-            <Tab label="Item Four" />
-            <Tab label="Item Five" />
-            <Tab label="Item Six" />
-            <Tab label="Item Seven" />
-          </Tabs>
+          <div>
+            <Tabs value={value} onChange={handleChange} variant="fullWidth">
+              {tabContent.map((tab, index) => (
+                <Tab key={index} label={tab.label} />
+              ))}
+            </Tabs>
+
+            <div style={{ marginTop: '20px' }}>
+              {tabContent.map((tab, index) => (
+                <Box
+                  key={index}
+                  sx={{
+                    display: value === index ? 'block' : 'none',
+                  }}
+                >
+                  <Grid container justifyContent="space-between" alignItems="center">
+                    <Grid item xs={6}>
+                      <Typography variant="h5">{tab.text}</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <img src={tab.img} alt="description" width="200" />
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Button variant="contained" endIcon={tab.icon}>
+                        Button
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Box>
+              ))}
+            </div>
+          </div>
         </Box>
+
         <div className="text-lg mt-8">
           <Typography variant="h2" align="center" gutterBottom>
             Почему нас выбирают
@@ -109,7 +141,7 @@ export default function MainPage(): JSX.Element {
           <Grid container spacing={3}>
             <Grid item xs={12} sm={4}>
               <img
-                src="https://vdecret.com/wp-content/uploads/4c01dcbba49a57535b133bd7ae2895e8-700x378.png"
+                src="https://images-na.ssl-images-amazon.com/images/I/91TpDKhj0eL._AC_SL1500_.jpg"
                 alt="description"
                 width="250"
                 height="250"
@@ -125,7 +157,7 @@ export default function MainPage(): JSX.Element {
             </Grid>
             <Grid item xs={12} sm={4}>
               <img
-                src="https://fixedratemoving.com/wp-content/uploads/2019/04/Movers.jpg"
+                src="https://30.img.avito.st/image/1/1.U7_TCraB_1alrw1Q-VNhzxyp-1Bxq_9QFsr7UKWvDVBlrfNSZav_Eg.Y2ZEvGfySPyfkoIRqs7fBmcevcmqtz1w8tNl9ia0uiA"
                 alt="description"
                 width="250"
                 height="250"
@@ -157,52 +189,75 @@ export default function MainPage(): JSX.Element {
             </Grid>
           </Grid>
         </div>
+
         <div className="text-lg mt-8">
           <Typography variant="h2" align="center" gutterBottom>
             Этапы проведения переезда
           </Typography>
 
-          <Grid container spacing={3}>
+          <Grid container spacing={4}>
             <Grid item xs={12} sm={3}>
-              <img
-                src="https://avatars.mds.yandex.net/get-ydo/2426891/2a000001737cbadc1566f63fef5011c47f84/diploma"
-                alt="description"
-                width="250"
-                height="400"
-              />
-              <Typography align="center">
-                Звоните нам по телефону или оставляйте онлайн-заявку на сайте.
-              </Typography>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                }}
+              >
+                <PhonelinkRingTwoToneIcon sx={{ width: 100, height: 400 }} />
+                <Typography align="center" style={{ fontWeight: 'bold' }}>
+                  Звоните нам по телефону или оставляйте онлайн-заявку на сайте.
+                </Typography>
+              </div>
             </Grid>
+
             <Grid item xs={12} sm={3}>
-              <img
-                src="https://avatars.mds.yandex.net/get-ydo/2426891/2a000001737cbadc1566f63fef5011c47f84/diploma"
-                alt="description"
-                width="250"
-                height="400"
-              />
-              <Typography align="center">
-                Проводим расчёт стоимости, оформляем заказ, подписываем договор.
-              </Typography>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                }}
+              >
+                <RequestQuoteTwoToneIcon sx={{ width: 100, height: 400 }} />
+                <Typography align="center" style={{ fontWeight: 'bold' }}>
+                  Проводим расчёт стоимости, оформляем заказ, подписываем договор.
+                </Typography>
+              </div>
             </Grid>
+
             <Grid item xs={12} sm={3}>
-              <img
-                src="https://avatars.mds.yandex.net/get-ydo/2426891/2a000001737cbadc1566f63fef5011c47f84/diploma"
-                alt="description"
-                width="250"
-                height="400"
-              />
-              <Typography align="center">
-                Выполняем переезд, если нужно разбираем-собираем, упаковываем, расставляем.
-              </Typography>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                }}
+              >
+                <LocalShippingTwoToneIcon sx={{ width: 100, height: 400 }} />
+                <Typography align="center" style={{ fontWeight: 'bold' }}>
+                  Выполняем переезд, если нужно разбираем-собираем, упаковываем, расставляем.
+                </Typography>
+              </div>
             </Grid>
+
             <Grid item xs={12} sm={3}>
-              <img
-                src="https://www.pngitem.com/middle/mxxoxR_transparent-phone-vector-png-vector-graphics-png-download/"
-                alt="description"
-                width="250"
-                height="400"
-              />
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                }}
+              >
+                <RedeemTwoToneIcon sx={{ width: 100, height: 400 }} />
+                <Typography align="center" style={{ fontWeight: 'bold' }}>
+                  Принимаете нашу работу и производите оплату удобным для Вас способом.
+                </Typography>
+              </div>
             </Grid>
           </Grid>
         </div>
@@ -210,4 +265,3 @@ export default function MainPage(): JSX.Element {
     </ThemeProvider>
   );
 }
-ё
